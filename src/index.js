3f96a767a7987f641419bbdc23ead54ca48d97fb
hermsale <hermsale@gmail.com>
2022-10-24 15:07:31 -0300

    se definio el menu colapsable y sus items

--- src/index.js
+++
@@ -1,7 +0,0 @@
-buttonHamburguesa = document.querySelector('.navbar-toggler')
-navbarcollapse = document.querySelector('.navbar-collapse');
-
-buttonHamburguesa.addEventListener('click', () =>{
-    console.log('boton presionado');
-    buttonHamburguesa.classList.aria-expanded('false');
-})
